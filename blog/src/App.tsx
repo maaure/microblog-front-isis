624739051452ec27823201059b430e4617f13f86
IsisAriele <isisariele1@gmail.com>
2024-05-03 10:15:34 -0300

    feat: adicionar componente header. (#7)

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,63 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Card from "./components/Card";
+import Feed from "./pages/Feed";
 
 function App() {
-	const [numero, setNumero] = useState<number>(0);
-
-	function decrescimo() {
-		setNumero(numero - 1);
-	}
-
 	useEffect(() => {
 		console.log("Olá useEffect");
 	}, []);
 
 	return (
 		<>
-			<Button
-				action={decrescimo}
-				label="Entrar"
-				className="br-sign-in mt-3 mt-sm-0 ml-sm-3"
-				icon={
-					<i
-						className="fas fa-user"
-						aria-hidden="true"
-					></i>
-				}
-			/>
-
-			<Card
-				title="Como a internet funciona?"
-				name="Demetria Devonne Lovato"
-				description="Lorem ipsum dolor sit, amet consectetur adipisicing
-				elit. Tempore perferendis nam porro atque ex at,
-				numquam non optio ab eveniet error vel ad
-				exercitationem, earum et fugiat recusandae harum?
-				Assumenda. Lorem ipsum dolor sit, amet consectetur
-				adipisicing elit. Tempore perferendis nam porro
-				atque ex at, numquam non optio ab eveniet error vel
-				ad exercitationem, earum et fugiat recusandae harum?
-				Assumenda. Lorem ipsum dolor sit, amet consectetur adipisicing
-				elit. Tempore perferendis nam porro atque ex at,
-				numquam non optio ab eveniet error vel ad
-				exercitationem, earum et fugiat recusandae harum?
-				Assumenda. Lorem ipsum dolor sit, amet consectetur
-				adipisicing elit. Tempore perferendis nam porro
-				atque ex at, numquam non optio ab eveniet error vel
-				ad exercitationem, earum et fugiat recusandae harum?
-				Assumenda. Lorem ipsum dolor sit, amet consectetur adipisicing
-				elit. Tempore perferendis nam porro atque ex at,
-				numquam non optio ab eveniet error vel ad
-				exercitationem, earum et fugiat recusandae harum?
-				Assumenda. Lorem ipsum dolor sit, amet consectetur
-				adipisicing elit. Tempore perferendis nam porro
-				atque ex at, numquam non optio ab eveniet error vel
-				ad exercitationem, earum et fugiat recusandae harum?
-				Assumenda."
-				image="https://www.w3schools.com/w3images/avatar6.png"
-			/>
+			<Feed />
 		</>
 	);
 }
